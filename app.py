9b18428f17af9afab4687be85bc98b1bd9b5b75b
Nora <nora@Noras-MacBook-Pro.local>
2022-12-24 14:14:45 +0300

    initial project setup

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+# from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello World! :)</h11>"
+    return "<h1>Hello World! :)</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
